f5a3a5328ba4b7d9925706e027ef59c8b6c25cc6
Tiberiu Dascal <tiberiu.purcel@gmail.com>
2022-10-01 01:16:43 +0300

    Am terminat ruta de adaugare/modificare si stergere

--- flowbite-flask/app.py
+++ flowbite-flask/app.py
@@ -6,7 +6,7 @@ from forms import AdaugaForm, ModificaForm
 
 right_now = datetime.datetime.now()
 
-mysql_db = MySQLDatabase('camine', user='root', password='', host='127.0.0.1', port=3306, charset='utf8mb4')
+mysql_db = MySQLDatabase('camine', user='root', password='AnaFilipDelia2022!', host='127.0.0.1', port=3306, charset='utf8mb4')
 
 class BaseModel(Model):
     class Meta:
@@ -37,7 +37,16 @@ def index():
 def add():
     form = AdaugaForm()
     if form.validate_on_submit():
-        print(f'{form.nume.data}, {form.telefon.data}, {form.site.data}, {form.pret.data}, {form.adresa.data}, {form.note.data}, {form.verificat.data}');
+        camin = Camin.create(nume=form.nume.data, 
+                             adresa=form.adresa.data,
+                             site=form.site.data,
+                             telefon=form.telefon.data,
+                             pret=form.pret.data,
+                             note=form.note.data,
+                             verificat=int(form.verificat.data),
+                             adaugat=datetime.datetime.now())
+        camin.save()
+        print(camin.id)
         flash(f'Camin "{form.nume.data}" adaugat cu success', 'success')
         return redirect(url_for('index'))
     return render_template("add.html", form=form)
@@ -45,11 +54,30 @@ def add():
 @app.route('/update/<id>', methods=['GET', 'POST'])
 def update(id):
     form = ModificaForm()
-    camine = Camin.select()
     camin = Camin.select().where(Camin.id == id).get()
-    if request.method == "POST":
-        pass
-    return render_template("update.html", camin=camin, camine=camine, form=form)
+    if form.validate_on_submit():
+        camin.nume=form.nume.data 
+        camin.adresa=form.adresa.data
+        camin.site=form.site.data
+        camin.telefon=form.telefon.data
+        camin.pret=form.pret.data
+        camin.note=form.note.data
+        camin.verificat=int(form.verificat.data)
+        camin.adaugat=datetime.datetime.now()
+        camin.save()
+        flash(f'Camin "{form.nume.data}" modificat cu success', 'success')
+        return redirect(url_for('index'))
+
+    return render_template("update.html", camin=camin, form=form)
+
+@app.route('/delete/<id>', methods=['GET', 'POST'])
+def delete(id):
+    camin = Camin.select().where(Camin.id == id).get()
+    nume = camin.nume
+    camin.delete_instance()
+    flash(f'Caminul {nume} a fost sters cu success', 'success')
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
