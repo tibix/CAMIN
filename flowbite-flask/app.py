6713305922edab93c90e82599acb38186988ccec
Dascal Tiberiu Claudiu <tiberiu.purcel@gmail.com>
2022-09-30 18:26:29 +0300

    Adaugat logica pentru validare

--- flowbite-flask/app.py
+++ flowbite-flask/app.py
@@ -1,12 +1,12 @@
 import datetime
 import re
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, flash, redirect
 from peewee import MySQLDatabase, Model, CharField, TextField, BooleanField, DateTimeField, IntegerField, OperationalError
 from forms import AdaugaForm, ModificaForm
 
 right_now = datetime.datetime.now()
 
-mysql_db = MySQLDatabase('camine', user='root', password='AnaFilipDelia2022!', host='127.0.0.1', port=3306, charset='utf8mb4')
+mysql_db = MySQLDatabase('camine', user='root', password='', host='127.0.0.1', port=3306, charset='utf8mb4')
 
 class BaseModel(Model):
     class Meta:
@@ -36,18 +36,10 @@ def index():
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     form = AdaugaForm()
-    if request.method == "POST":
-        print("post data:")
-        # nume = request.form['nume']
-        # adresa = request.form['adresa']
-        # site = request.form['site']
-        # telefon = request.form['telefon']
-        # pret = request.form['pret']
-        # verificat = request.form['verificat']
-        # note = request.form['note']
-
-        # print(f'Nume: {nume}\nAdresa: {adresa}\nSite: {site}\nTelefon: {telefon}\nPret: {pret}\nNote: {note}\n')
-        print(request.form.getlist('verificat'))
+    if form.validate_on_submit():
+        print(f'{form.nume.data}, {form.telefon.data}, {form.site.data}, {form.pret.data}, {form.adresa.data}, {form.note.data}, {form.verificat.data}');
+        flash(f'Camin "{form.nume.data}" adaugat cu success', 'success')
+        return redirect(url_for('index'))
     return render_template("add.html", form=form)
 
 @app.route('/update/<id>', methods=['GET', 'POST'])
