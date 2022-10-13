9dee43f2bf30437c004efbd9969f4c5eda137321
Dascal Tiberiu Claudiu <tiberiu.purcel@gmail.com>
2022-10-13 18:11:20 +0300

    changed folder structure

--- flowbite-flask/config.py
+++
@@ -1,8 +0,0 @@
-class Secrets():
-    db = "camine"
-    host = "itdev.ro"
-    user = "camin"
-    password = "C4m1n2022!"
-    port = 3306
-    charset = "utf8mb4"
-    app_secret = "fa11dd842bf8374664a9e72fb4eecabe26b3416a21910dffa4"
